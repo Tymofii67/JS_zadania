67b1585dbfdd293313b0b2bf497e3e1aacdde33a
Tymofii67 <coppilkagg@gmail.com>
2026-09-16 16:29:55 +0200

    Add files via upload

---
+++ kova_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("mam numer w dzienniku 8".fontcolor("black")+"<br>"); 
+document.write("Pierwszy wiersz to:   9 Nie Tak 6 ".fontcolor("black")+"<br>"); 
+document.write("Drugi wiersz to:      9 Nie Tak 6 ".fontcolor("black")+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("green").strike().fontsize(6)+"<br>"); 
+document.write(document.lastModified.fontcolor("green").strike().fontsize(6)); 
